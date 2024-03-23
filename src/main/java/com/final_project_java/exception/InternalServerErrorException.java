58c261560006f1fad9f703bd13aa3fe35d2b291f
adrian-ionescu-1 <adrian.george2505@yahoo.com>
2024-03-23 09:39:03 +0200

    1 update

--- src/main/java/com/final_project_java/exception/InternalServerErrorException.java
+++ src/main/java/com/final_project_java/exception/InternalServerErrorException.java
@@ -3,7 +3,7 @@ package com.final_project_java.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR) // 500
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class InternalServerErrorException extends RuntimeException {
     public InternalServerErrorException(String message) {
         super(message);
--- src/main/java/com/final_project_java/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/final_project_java/service/impl/CustomerServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
     public CustomerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
